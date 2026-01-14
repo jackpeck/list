35797375f587037a89bbad70f9bb9c7295ca6b0c
x <>
2026-01-14 14:33:07 -0800

    tuning to try to get it to generalise (not working yet). no biases, full batch training

--- encode_items_sequentially_parameterize_list_length.py
+++ encode_items_sequentially_parameterize_list_length.py
@@ -2,8 +2,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-k = 5
-list_len = 2
+k = 37
+list_len = 3
 
 torch.manual_seed(0)
 
@@ -14,8 +14,8 @@ class Model(nn.Module):
         self.dim = dim
 
         self.l1 = nn.Embedding(k, dim)
-        self.l2 = nn.Linear(dim, dim)
-        self.l3 = nn.Linear(dim, k)
+        self.l2 = nn.Linear(dim, dim, bias=False)
+        self.l3 = nn.Linear(dim, k, bias=False)
         self.embed_attribute_index = nn.Embedding(list_len, dim)
 
     def encoder(self, item, prior_state):
@@ -52,18 +52,22 @@ targets = inputs[torch.arange(inputs.size(0)), inputs[:, list_len]]
 # out = model.encoder(inputs[:, 1], out)
 # out = model.encoder(inputs[:, 2], out)
 
-out = model.enc_seq(inputs)
-out = model.decoder(out, inputs[:, list_len])
+# out = model.enc_seq(inputs)
+# out = model.decoder(out, inputs[:, list_len])
 
 train_mask = ~(inputs[:, 1] == 0)
-print(train_mask)
+print(train_mask, train_mask.sum(), inputs.size(0))
 inputs_train = inputs[train_mask]
 targets_train = targets[train_mask]
 
-batch_sz = 4
-n_steps = 10000
+batch_sz = 999999999
+n_steps = 1000000
 
-optimizer = torch.optim.AdamW(model.parameters(), lr=1e-2, weight_decay=0.1)
+optimizer = torch.optim.AdamW(
+    model.parameters(),
+    lr=1e-3,
+    weight_decay=1.0,  # , betas=(0.9, 0.98)
+)
 
 for step in range(n_steps):
     optimizer.zero_grad()
@@ -77,8 +81,10 @@ for step in range(n_steps):
     loss = F.cross_entropy(out, targets_sample)
     loss.backward()
     optimizer.step()
-    # if step % 100 == 0:
-    #     print(step, loss.item())
+    if step % 100 == 0:
+        print(
+            step, loss.item(), (out.argmax(-1) == targets_sample).float().mean().item()
+        )
 
     if step % 1000 == 0 or step == n_steps - 1:
         with torch.no_grad():
@@ -89,7 +95,21 @@ for step in range(n_steps):
             out = model.enc_seq(inputs_train)
             out = model.decoder(out, inputs_train[:, list_len])
             train_loss = F.cross_entropy(out, targets_train)
-            print(step, "val loss", val_loss.item(), "train loss", train_loss.item())
+
+            # out = model.enc_seq(inputs[train_mask])
+            # out = model.decoder(out, inputs[train_mask][:, list_len])
+            # loss = F.cross_entropy(out, targets[train_mask])
+            # # print(loss)
+
+            out = model.enc_seq(inputs[~train_mask])
+            out = model.decoder(out, inputs[~train_mask][:, list_len])
+            test_loss = F.cross_entropy(out, targets[~train_mask])
+            # print(test_loss)
+
+            print(
+                step,
+                f"val_loss={val_loss.item():.6f} train_loss={train_loss.item():.6f} test_loss={test_loss.item():.6f}",
+            )
 
     # out = model.enc_seq(inputs_train)
     # out = model.decoder(out, inputs_train[:, list_len])
